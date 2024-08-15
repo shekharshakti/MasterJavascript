c55d6e6e786e7dcd988095bc8cbd7f27352b3ec2
Shekhar Shakti <119730877+shekharshakti@users.noreply.github.com>
2024-08-15 06:59:53 +0000

    done

---
+++ 01.Beginner/01basic.js
@@ -0,0 +1,36 @@
+// javascript is a verstile and widely
+// used programming language that play 
+// a crucial role in web-development 
+// it enable developers to create 
+// interact and dynamic element within 
+// webpage ,enhancing the user 
+// experience by adding functionality 
+// that responds to user action 
+// javascriot is primarly executed in 
+// web browser.but its also used in 
+// other environment like server -side
+// scripting (node.js)  
+console.log("hello javascript");
+// ---------- CONSOLE ----------
+console.log("Hello World");
+// log
+// clg
+// console.log(console); // console object
+
+console.error("This is an ERROR");
+console.warn("Warning");
+console.table({ name: "Shekhar ", age: 20, location: ["India"] });
+
+// ---------- COMMENTS ----------
+
+// This is one line comment
+/*
+    This is 
+    multi-line 
+        comment
+*/
+/** Unordered Comment
+ * bla
+ * bla
+ * bla
+ */
