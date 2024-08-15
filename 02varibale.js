24decc09f3adf67d3f7c982b5c9d0bea9fd31bec
Shekhar Shakti <119730877+shekharshakti@users.noreply.github.com>
2024-08-15 11:07:36 +0000

    donw

--- 02varibale.js
+++
@@ -1,7 +0,0 @@
-// (reserved keyword): (variable name): (type of value)
-
-let variable; // undefined
-let name = "Shekhar";
-console.log(name);
-const channelName = "LOL";
-console.log(channelName);
