f8a1bbb0c941bb8e68a0aa97cb92903ba6e933f1
Shekhar Shakti <119730877+shekharshakti@users.noreply.github.com>
2024-08-15 11:28:47 +0000

    JavaScript Course

---
+++ 02.CantrolFlow/03 Loops.js
@@ -0,0 +1,53 @@
+// for (initialExpression; condition; incrementExpression) {}
+
+for (let i = 0; i <= 5; i++) {
+    console.log("Hello World", i);
+  }
+  
+  // EXERCISE
+  // ... (let ; = ; < ; ) {
+  //   console.log(i);
+  // }
+
+  // Print your name & numbers from 0 through 1000
+
+for (let i = 0; i <= 1000; i++) {
+    console.log("HuXn WebDev", i);
+  }
+
+
+  
+// while (condition) {
+//   // code block to be executed
+// }
+
+// ----------------
+let i = 1;
+
+while (i <= 5) {
+  console.log("Hello World", i);
+  i++;
+}
+// ----------------
+
+
+
+// Print "Your Name" 100 times to the console using while loop
+// Iteration will start from 10.
+
+let I = 10;
+
+while (I <= 100) {
+  console.log("HuXn WebDev");
+  I++;
+}
+
+
+//Do-while
+
+let i1 = 1;
+
+do {
+  console.log("Hello World", i);
+  i1++;
+} while (i1 <= 5);
