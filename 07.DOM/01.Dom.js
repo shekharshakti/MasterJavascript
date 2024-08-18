0930480d2b24d0367f5000084effc1650aa76af1
Shekhar Shakti <119730877+shekharshakti@users.noreply.github.com>
2024-08-18 18:00:02 +0000

    Jvascript

---
+++ 07.DOM/01.Dom.js
@@ -0,0 +1,16 @@
+/*
+*************DOM*************
+the document object model is a 
+programming interface for web 
+document . it represnt the page 
+so that programs can change the 
+document structure ,style,and content
+
+*********Types of DOm************
+
+1- getElementByID().
+2- getElementBynName().
+3- getElementByTagName().
+4- getElementByClassName().
+5- document.querySelectorAll().
+*/
